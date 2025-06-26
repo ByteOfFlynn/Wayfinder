2bf98cacebfb5239382df25d62ee76d96d43f903
Ian Flynn <122494717+ByteOfFlynn@users.noreply.github.com>
2025-06-26 18:48:24 -0400

    Delete java/com/example/wayfinder directory

--- java/com/example/wayfinder/Clue.kt
+++
@@ -1,9 +0,0 @@
-package com.example.wayfinder
-
-data class Clue(
-    val clueText: String,
-    val hints: List<String>,
-    val latitude: Double,
-    val longitude: Double,
-    val locationInfo: String
-)
